6a4b8cfc9b4ea2d0a73ae4743e53753105c8a372
Sukhseerat Kaur <karanjitkaur78@gmail.com>
2021-10-08 23:06:01 +0530

    authorization

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,89 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
+const {
+  userAuth,
+  userLogin,
+  checkRole,
+  userRegister,
+  // serializeUser,
+} = require("../utils/auth.js");
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
 });
+
+//Registeration
+router.post("/register/:role", async (req, res) => {
+  const { role } = req.params;
+  await userRegister(req.body, role, res);
+});
+
+// router.post("/team-leader-register", async (req, res) => {
+//   await userRegister(req.body, "team-leader", res);
+// });
+
+// router.post("/vice-team-leader-register", async (req, res) => {
+//   await userRegister(req.body, "vice-team-leader", res);
+// });
+
+// router.post("/batch-leader-register", async (req, res) => {
+//   await userRegister(req.body, "batch-leader", res);
+// });
+
+//login
+router.post("/login/:role", async (req, res) => {
+  const { role } = req.params;
+  await userLogin(req.body, role, res);
+});
+
+// router.post("/team-leader-login", async (req, res) => {
+//   await userLogin(req.body, "team-leader", res);
+// });
+
+// router.post("/vice-team-leader-login", async (req, res) => {
+//   await userLogin(req.body, "vice-team-leader", res);
+// });
+
+// router.post("/batch-leader-login", async (req, res) => {
+//   await userLogin(req.body, "batch-leader", res);
+// });
+
+//protected routes
+router.get(
+  "/student-protected",
+  userAuth,
+  checkRole(["student"]),
+  async (req, res) => {
+    return res.send("only students can access this route");
+  }
+);
+
+router.get(
+  "/team-leader-protected",
+  userAuth,
+  checkRole(["team-leader"]),
+  async (req, res) => {
+    return res.send("only team-leaders can access this route");
+  }
+);
+
+router.get(
+  "/vice-team-leader-protected",
+  userAuth,
+  checkRole(["vice-team-leader"]),
+  async (req, res) => {
+    return res.send("only vice-team-leader can access this route");
+  }
+);
+
+router.get(
+  "/batch-leader-protected",
+  userAuth,
+  checkRole(["batch-leader"]),
+  async (req, res) => {
+    return res.send("only batch-leader can access this route");
+  }
+);
 
 module.exports = router;
